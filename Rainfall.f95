8b4672558cf39ce774db5711d72e6dec604f6ff9
malfyr <malfyr123@gmail.com>
2023-11-17 14:19:25 +0200

    c5 - Created subroutine to read file

--- Rainfall.f95
+++ Rainfall.f95
@@ -1,32 +1,61 @@
 program rainfall
-implicit none
-integer,parameter,save::Nmax=50
-integer::x,i,st,Nmax,rain(1,Nmax)
-character(20)::s,town(1,Nmax)
-town='0'
-rain=0
-
-
-open(10,file='data.txt',iostat=st)
-if (st/=0) then
-print*, 'To arxeio den yparxei'
-else
-print*, 'To arxeio yparxei'
-
-end if
-read(10,'(A)',iostat=st)s
+implicit none 
+integer::x,i,st,j,N
+integer, parameter, save ::Nmax=50
+character(20)::s
+integer::rain(1,Nmax),R(1,Nmax)
+character(15)::town(1,Nmax),T(1,Nmax)
 
 
 
- do i= 1,Nmax
-   read(10,*,iostat=st) s,x
-   if (st==-1) exit
-  print *,s,x
-  town(1,i)=s
-  rain(1,i)=x
- end do
- print*,rain
- print *,town
-!hi
+  call ReadData(Nmax,'data.txt',T,R,N)
+  print*,N,T,R
+
+
+
+
+
+
  close(10)
-end
+
+
+ contains 
+
+subroutine ReadData (Nmax,FileName,T,R,N)
+Integer, intent(out)::N
+Integer, intent(in)::Nmax
+character(*),intent(in)::FileName
+character(15),intent(out)::T(1,Nmax)
+integer,intent(out)::R(1,Nmax)
+integer::st,i
+
+N=Nmax
+T='0'
+R=0
+
+ 
+open(10,file=Filename,iostat=st)
+
+
+    read(10,*,iostat=st)
+  do i=1,Nmax
+   read(10,*,iostat=st) s,x
+    if (st==-1) exit
+      print *,s,x
+       T(1,i)=s
+         R(1,i)=x
+  end do
+  do i=1,Nmax
+   if  (R(1,i)==0) N=N-1
+     
+      end do 
+      
+
+
+end subroutine ReadData
+
+
+
+
+ 
+end 
